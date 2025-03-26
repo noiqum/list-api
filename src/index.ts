e71c02531bfc4af77bffb39bcc8193373220eadd
noiqum <onurcoskun99@gmail.com>
2025-03-26 23:53:32 +0300

    vercel deploy config

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import authRouter from "./routes/authRoutes"
 import todoRouter from "./routes/todoRoutes"
 import cookieParser from "cookie-parser"
+import cors from "cors"
+import { VercelRequest, VercelResponse } from "@vercel/node"
 
 declare global {
     namespace Express {
@@ -15,8 +17,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: 10000 }))
 app.use(cookieParser())
-
-app.get("/", (req: Request, res: Response) => {
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true
+    })
+)
+app.get("/", async (req: Request, res: Response) => {
     res.json({
         status: "success",
         message: "server is working"
@@ -32,3 +39,5 @@ const PORT = process.env.PORT || "8080"
 app.listen(PORT, async () => {
     console.log(`server is running on port ${PORT}`)
 })
+
+export default app;
